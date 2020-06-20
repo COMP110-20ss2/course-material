44343464b25c5831e9b72e008f590530138bf5c9
Kris Jordan <krisjordan@gmail.com>
2020-06-20 18:06:40 -0400

    Initial organization of student repository.

---
+++ comp110/demos/black_triangle.py
@@ -0,0 +1,14 @@
+"""Draw a black triangle with turtle graphics."""
+
+from turtle import begin_fill, done, end_fill, forward, left
+
+
+begin_fill()
+forward(100)
+left(120)
+forward(100)
+left(120)
+forward(100)
+left(120)
+end_fill()
+done()
